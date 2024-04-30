1fbdda2c7cb7cbb23b04be7d6542d7ad10aa44eb
jamakr4 <68536642+jamakr4@users.noreply.github.com>
2024-05-01 00:14:54 +0200

    upload

--- scripts.js
+++ scripts.js
@@ -13,3 +13,21 @@ function fadeOutAside() {
     aside.classList.remove('fade-out'); // Remove the fade-out class
     document.getElementById('b').style.display = 'none'; // Hide the 'Show' button
   }
+
+
+
+
+// Ab hier Funktionen für den Taschenrechner
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ 
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
